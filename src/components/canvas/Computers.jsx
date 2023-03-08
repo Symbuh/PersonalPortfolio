4fa7acf093d5fa10c276edb3bd869dbae9d8922c
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2023-03-08 01:10:14 -0800

    Public folder isn't showing up in the browser properly, trying to commit to fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,22 +1,30 @@
-import { Suspense, useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
 import CanvasLoader from '../Loader';
 
 // My first 3js React3Fiber Canvas
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
 
-  console.log('computer', computer);
-
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, 1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
@@ -41,4 +49,4 @@ const ComputersCanvas = () => {
   </Canvas>;
 };
 
-export default Computers;
+export default ComputersCanvas;
