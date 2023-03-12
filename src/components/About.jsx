ffec764a42c475bce50377a4f9be0391097b7da7
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2023-03-12 08:17:48 -0700

    Fixed about cards and added Higher Order Component to handle centering, formatting, and routing of all sections.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -51,9 +51,9 @@ const About = () => {
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
-        {services.map((service, index) => {
-          <ServiceCard key={service.title} index={index} {...services} />;
-        })}
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
       </div>
     </>
   );
