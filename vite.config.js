150e5d03aae5dbd35748e098695da99239bc9906
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2023-03-17 14:11:39 -0700

    Removed gltf compression to test performance difference

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import compression from 'vite-plugin-compression';
 
 export default defineConfig({
   publicDir: 'public',
-  plugins: [
-    react(),
-    compression({
-      ext: '.gz',
-      algorithm: 'gzip',
-      filter: /\.(bin|gltf)$/,
-      deleteOriginFile: true,
-    }),
-  ],
+  plugins: [react()],
   assetFileTypes: ['css', 'svg', 'png', 'jpeg', 'gif', 'bin', 'gltf'],
 });
